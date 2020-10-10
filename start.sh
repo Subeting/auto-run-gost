f4b9fcbf4a59175aba8dc6cd9b8a86dfc8155e5c
Subeting <68261543+Subeting@users.noreply.github.com>
2020-10-10 13:56:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -31,7 +31,7 @@ After=network-online.target
 Wants=network-online.target
 
 [Service]
-ExecStart=/usr/bin/nohup /bin/bash /home/gost/*.sh >/dev/null 2>&1 &
+ExecStart=/bin/bash /home/gost/*.sh
 LimitNOFILE=100000
 Restart=always
 RestartSec=60
